0b58e5fcc4a1f6f9970a36441a8b2fa0616fff5e
Niall MacCrann <niall.maccrann@gmail.com>
2018-03-12 16:30:03 -0400

    allow user to specify starting id in meds file

--- galsim/des/des_meds.py
+++ galsim/des/des_meds.py
@@ -458,8 +458,10 @@ class MEDSBuilder(galsim.config.OutputBuilder):
                 raise AttibuteError("MEDS files are not compatible with image type %s."%image_type)
 
         req = { 'nobjects' : int , 'nstamps_per_object' : int }
+        opt  = {'first_id' : int }
         ignore += [ 'file_name', 'dir', 'nfiles' ]
-        params = galsim.config.GetAllParams(config,base,ignore=ignore,req=req)[0]
+        params = galsim.config.GetAllParams(config,base,ignore=ignore,req=req, opt=opt)[0]
+        first_id = params.get('first_id', obj_num)
 
         nobjects = params['nobjects']
         nstamps_per_object = params['nstamps_per_object']
@@ -495,7 +497,7 @@ class MEDSBuilder(galsim.config.OutputBuilder):
                                       weight = weight_images[k1:k2],
                                       badpix = bpk,
                                       psf = psf_images[k1:k2],
-                                      id = obj_num + i,
+                                      id = first_id + i,
                                       cutout_row = cutout_rows[k1:k2],
                                       cutout_col = cutout_cols[k1:k2])
             obj_list.append(obj)
